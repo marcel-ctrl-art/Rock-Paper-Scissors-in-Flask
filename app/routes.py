34934fc42fa77580a12f2f1e5afa6a7ba8e8018c
Marcel <marceli.lagoda@gmail.com>
2020-02-11 05:21:42 +0100

    Add index view

--- app/routes.py
+++ app/routes.py
@@ -2,3 +2,8 @@ from app import app
 from app.forms import GameForm
 from flask import request, render_template
 
+
+@app.route('/')
+def index():
+    form = GameForm()
+    return render_template('index.html', form=form)
