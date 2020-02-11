ba7f177a0f848871040cf2e306a7932c4f78d9f8
Marcel <marceli.lagoda@gmail.com>
2020-02-11 22:12:20 +0100

    Update form description

--- app/forms.py
+++ app/forms.py
@@ -4,5 +4,5 @@ from wtforms.validators import DataRequired
 
 
 class GameForm(FlaskForm):
-    choice = SelectField('Your choice', choices=[('scissors', 'scissors'), ('rock', 'rock'), ('paper', 'paper')])
+    choice = SelectField('Please pick your tool:', choices=[('scissors', 'scissors'), ('rock', 'rock'), ('paper', 'paper')])
     submit = SubmitField('Submit')
