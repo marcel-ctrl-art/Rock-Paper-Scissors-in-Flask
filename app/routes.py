10310554c8e9748ec856c7e9bdec253a642bae11
Marcel <marceli.lagoda@gmail.com>
2020-02-11 05:24:09 +0100

    Add view winner

--- app/routes.py
+++ app/routes.py
@@ -7,3 +7,21 @@ from flask import request, render_template
 def index():
     form = GameForm()
     return render_template('index.html', form=form)
+
+
+@app.route('/winner', methods=['GET', 'POST'])
+def winner(choice=None):
+    human = request.form['choice']
+
+    CHOICES = ('paper', 'scissors', 'rock')
+    computer = random.choice(CHOICES)
+
+    winner = ways_to_win(request.form['choice'], computer)
+
+    return render_template(
+        'winner.html',
+        human=human,
+        computer=computer,
+        winner=winner
+
+    )
