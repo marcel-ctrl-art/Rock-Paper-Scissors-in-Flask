9b4c7ec724d46df6765ff163a7aa7d35fe2ac514
Marcel <marceli.lagoda@gmail.com>
2020-02-11 22:39:55 +0100

    Remove ways_to_win() from routes.py and import it instead

--- app/routes.py
+++ app/routes.py
@@ -2,29 +2,8 @@ from app import app
 from app.forms import GameForm
 from flask import request, render_template
 import random
+from app.winner import ways_to_win
 
-
-def ways_to_win(human, computer):
-    if human == computer:
-        return 'TIE'
-
-    elif human == 'paper' and computer == 'scissors':
-        return 'computer is a winner'
-
-    elif human == 'paper' and computer == 'rock':
-        return 'human is the winner'
-
-    elif human == 'scissors' and computer == 'paper':
-        return 'human is the winner'
-
-    elif human == 'scissors' and computer == 'rock':
-        return 'computer is the winner'
-
-    elif human == 'rock' and computer == 'paper':
-        return 'computer is the winner'
-
-    elif human == 'rock' and computer == 'scissors':
-        return 'human is the winner'
 
 @app.route('/')
 def index():
